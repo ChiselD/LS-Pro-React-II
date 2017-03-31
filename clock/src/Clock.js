46cdab4af675578a33675210340cc859cd8f1a40
ChiselD <chisel.delahanty@gmail.com>
2017-03-30 21:11:59 -0400

    Rewrite using componentDidMount and toLocaleTimeString; it works flawlessly now but I want to try to rewrite without toLocaleTimeString

--- clock/src/Clock.js
+++ clock/src/Clock.js
@@ -3,34 +3,51 @@ import './Clock.css';
 
 export default class Clock extends Component {
 
+// old version
+  // constructor(props) {
+  //   super(props);
+  //   this.updateTime = this.updateTime.bind(this);
+  //   let now = new Date();
+  //   this.state = {
+  //     hr: now.getHours(),
+  //     mn: now.getMinutes(),
+  //     sc: now.getSeconds()
+  //   };
+  // }
+
   constructor(props) {
     super(props);
     this.updateTime = this.updateTime.bind(this);
-    let now = new Date();
     this.state = {
-      hr: now.getHours(),
-      mn: now.getMinutes(),
-      sc: now.getSeconds()
+      now: new Date()
     };
   }
 
+// old version
+  // updateTime() {
+  //   this.setState({
+  //     hr: this.now.getHours(),
+  //     mn: this.now.getMinutes(),
+  //     sc: this.now.getSeconds()
+  //   });
+  // }
+
   updateTime() {
     this.setState({
-      hr: this.now.getHours(),
-      mn: this.now.getMinutes(),
-      sc: this.now.getSeconds()
+      now: new Date()
     });
   }
 
-  changeTime() {
+  componentDidMount() {
     setInterval(this.updateTime, 1000);
   }
 
   render() {
     return (
       <div className='clock'>
-        {this.state.hr}:{this.state.mn}:{this.state.sc}
+        {this.state.now.toLocaleTimeString()}
       </div>
     )
   }
+
 }
